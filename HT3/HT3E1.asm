87f657603f22519e8198b0e0b3f497e747c211ac
J4SSGG <jasg.sotogutierrez@gmail.com>
2018-09-27 15:56:04 -0600

    Solucionador parte 1 HT3

---
+++ HT3/HT3E1.asm
@@ -0,0 +1,144 @@
+.MODEL SMALL
+.DATA
+msgNum DB ' Numero: $'
+msgSuma DB ' Sumar: $'
+msgRestar DB ' Restar: $'
+msgMult DB ' Mutl: $'
+msgDiv DB ' Div: $'
+msgRes DB ' Res: $'
+numero1 DW ? 
+numero2 DW ?
+.CODE
+EJERCICIO1:
+    MOV AX, @DATA
+    MOV DS, AX
+    XOR AX, AX
+
+    ;Mostrar mensaje de numero
+    XOR DX, DX
+    XOR AX, AX
+    MOV DX, offset msgNum
+    MOV AH, 09h
+    INT 21H
+    
+    ; Leer dato1
+    XOR AX, AX
+    MOV AH, 01h
+    INT 21H
+    SUB AL, 30h ;usar solopp cuando se quiere el numero no el char
+    SUB AH, 01h ;limpiar AH para evitar cargar a numero valores incorrectos
+    MOV numero1, AX
+    
+    ;Mostrar mensaje de numero 
+    XOR DX, DX
+    XOR AX, AX
+    MOV DX, offset msgNum
+    MOV AH, 09h
+    INT 21H
+    
+    ; Leer dato2
+    XOR AX, AX
+    MOV AH, 01h
+    INT 21H
+    SUB AX, 30h
+    SUB AH, 01h ;limpiar AH para evitar cargar a numero valores incorrectos
+    MOV numero2, AX
+
+    
+    ;Mostrar mensaje suma
+    XOR DX, DX 
+    XOR AX, AX
+    MOV DX, offset msgSuma
+    MOV AH, 09h
+    INT 21H
+    
+    ; Sumar
+    XOR DX, DX
+    XOR AX, AX
+    MOV DX, numero1
+    ADD DX, numero2
+    ADD DX, 30h ; Para mostrar el char del decimal
+    MOV AH, 02h
+    INT 21H
+    
+    ;Mostrar mensaje resta 
+    XOR DX, DX
+    XOR AX, AX
+    MOV DX, offset msgRestar
+    MOV AH, 09h
+    INT 21H
+    
+    ;Restar
+    XOR DX, DX
+    XOR AX, AX
+    MOV DX, numero1
+    SUB DX, numero2
+    ADD DX, 30h
+    MOV AH, 02h
+    INT 21H
+    
+    
+    ;Mostrar mensaje mult 
+    XOR DX, DX
+    XOR AX, AX
+    MOV DX, offset msgMult
+    MOV AH, 09h
+    INT 21H
+    
+    ;Multiplicar
+    XOR DX, DX
+    XOR AX, AX
+    MOV AX, numero1
+    MUL numero2
+    MOV DX, AX
+    ADD DX, 30h ; Para mostrar el char del decimal
+    MOV AH, 02h
+    INT 21H
+    
+    
+    ;Mostrar mensaje div 
+    XOR DX, DX
+    XOR AX, AX
+    MOV DX, offset msgDiv
+    MOV AH, 09h
+    INT 21H
+    
+    ;Dividir
+    XOR DX, DX
+    XOR AX, AX
+    MOV AX, numero1
+    DIV numero2
+    MOV DX, AX
+    ADD DX, 30h
+    MOV AH, 02h
+    INT 21H
+    
+    ;Mostrar mensaje res
+    XOR DX, DX
+    XOR AX, AX
+    MOV DX, offset msgRes
+    MOV AH, 09h
+    INT 21H
+    
+    ;Dividir
+    XOR DX, DX
+    XOR AX, AX
+    MOV AX, numero1
+    DIV numero2
+    ADD DX, 30h
+    MOV AH, 02h
+    INT 21H
+ 
+    
+    ; Fin de programa
+     MOV AH,4Ch
+     INT 21h
+    
+    ; Fin de programa
+     MOV AH,4Ch
+     INT 21h
+     
+END EJERCICIO1
+.STACK
+
+ 
